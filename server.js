f453bd1424b2652fcc0e04457fdd0d8d0b090fe9
OmarHassan219 <ramo-3000@live.com>
2023-08-13 16:53:07 +0400

    go

--- server.js
+++ server.js
@@ -118,6 +118,6 @@ res.status(200).json({ success: true, message: "Email Sent" });
 }
 })
 
-const PORT = process.env.PORT || 4242;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Node server listening on port ${PORT}`));
