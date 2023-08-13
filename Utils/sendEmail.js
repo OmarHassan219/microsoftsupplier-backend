67a39c75014271baff0534a18b37a076670f7534
OmarHassan219 <ramo-3000@live.com>
2023-08-14 03:06:04 +0400

    sd

--- Utils/sendEmail.js
+++ Utils/sendEmail.js
@@ -3,8 +3,8 @@ const nodemailer = require("nodemailer");
 const sendEmail = async (subject, message, send_to, sent_from, reply_to) => {
   const transporter = nodemailer.createTransport({
     service:"hotmail",
-    // host: process.env.EMAIL_HOST,
-    // port: 587,
+    host: process.env.EMAIL_HOST,
+    port: 995,
     secureConnection: false, // true for 465, false for other ports
     auth: {
       user: process.env.EMAIL_USER,
